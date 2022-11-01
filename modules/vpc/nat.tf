8952b9ac18d324d412ac6b4ae15a24feb52fedb5
ragul28 <ragulh28@gmail.com>
2022-11-01 23:18:01 +0530

    VPC: add nat gw enable bool

--- modules/vpc/nat.tf
+++ modules/vpc/nat.tf
@@ -1,9 +1,12 @@
 # Nat gateway placed under public subnet to route to traffic to igw
 resource "aws_eip" "nat_gw_eip" {
-  vpc = true
+  count = var.enable_natgw == true ? 1 : 0
+  vpc   = true
 }
 
 resource "aws_nat_gateway" "nat_gw_public" {
-  allocation_id = aws_eip.nat_gw_eip.id
-  subnet_id     = aws_subnet.public_subnet.0.id
+  count = var.enable_natgw == true ? 1 : 0
+
+  allocation_id = var.enable_natgw == true ? aws_eip.nat_gw_eip.0.id : null
+  subnet_id     = var.enable_natgw == true ? aws_subnet.public_subnet.0.id : null
 }
