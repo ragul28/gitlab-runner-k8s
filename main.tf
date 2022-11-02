c3ad3bf47ca5190868e0627eb3dcea2c63bf6663
ragul28 <ragulh28@gmail.com>
2022-11-02 23:55:21 +0530

    Add helm module on main

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "aws-vpc" {
-  source                    = "./modules/vpc"
-  project                   = var.project
-  main_vpc_cidr_block       = var.main_vpc_cidr_block
-  az_set                    = var.az_set
-  public_subnets            = var.public_subnets
-  private_subnets           = var.private_subnets
-  enable_natgw              = var.enable_natgw
+  source              = "./modules/vpc"
+  project             = var.project
+  main_vpc_cidr_block = var.main_vpc_cidr_block
+  az_set              = var.az_set
+  public_subnets      = var.public_subnets
+  private_subnets     = var.private_subnets
+  enable_natgw        = var.enable_natgw
 }
 
 
@@ -24,4 +24,15 @@ module "aws-s3" {
   source      = "./modules/s3"
   project     = var.project
   environment = var.environment
+}
+
+module "helm" {
+  depends_on              = [module.aws-eks.name]
+  source                  = "./modules/helm"
+  gitlab_url              = var.gitlab_url
+  gitlab_reg_token_map    = var.gitlab_reg_token_map
+  runner_concurrent_count = var.runner_concurrent_count
+  runner_helm_version     = var.runner_helm_version
+  aws_region              = var.aws_region
+  s3_bucket_name          = module.aws-s3.s3_bucket_name
 }
